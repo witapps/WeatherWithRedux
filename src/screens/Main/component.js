bfad5822c2a7a7cb5a6f5623cdeb7d83f7b4ff85
Oleksij Zadvornij <oleksij.zadvornij@jointoit.com>
2019-06-08 01:44:00 +0300

    link android library

--- src/screens/Main/component.js
+++ src/screens/Main/component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
+import Permissions from 'react-native-permissions';
 
 import styles from './styles';
 
@@ -23,30 +24,46 @@ export default class component extends Component {
   };
 
   sendWeather = () => {
-    Geolocation.getCurrentPosition(
-      position => {
-        const { latitude, longitude } = position.coords;
-        this.props.getWeather({
-          latitude,
-          longitude
-        });
-      },
-      error => {
-        console.log(error.code, error.message);
-      },
-      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-    );
+    Permissions.request('location').then(response => {
+      if (response === 'authorized') {
+        console.log(response);
+        Geolocation.getCurrentPosition(
+          position => {
+            console.log(position);
+            const { latitude, longitude } = position.coords;
+            this.props.getWeather({
+              latitude,
+              longitude
+            });
+          },
+          error => {
+            console.log(error.code, error.message);
+          },
+          { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+        );
+      }
+    });
   };
 
-  render() {
-    const { temp, humidity } = this.props.weather.main;
-    return (
-      <View style={styles.wrap}>
-        <View style={styles.flex} />
+  renderTemp = () => {
+    const { isStart } = this.props.weather;
+    if (isStart) {
+      const { temp, humidity } = this.props.weather.main;
+      return (
         <View>
           <Text style={styles.text}>Temperature: {temp}°С</Text>
           <Text style={styles.text}>Humidity: {humidity}%</Text>
         </View>
+      );
+    }
+    // return <Text>dsada</Text>;
+  };
+
+  render() {
+    return (
+      <View style={styles.wrap}>
+        <View style={styles.flex} />
+        {this.renderTemp()}
         <View style={styles.flex} />
         <Text style={styles.button} onPress={this.onPressRefresh}>
           Refresh
